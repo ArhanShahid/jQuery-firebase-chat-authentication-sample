80eba9e6131952bf4c470d36fd6bf26eadca5881
Arhan Shahid <arhanshahid@gmail.com>
2016-01-04 09:30:36 +0500

    jQuery-firebase-chat-authentication-sample

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var ref = new Firebase("<< FIREBASE APP URL >>");
+
+
+//ref.createUser({
+//    email: "<< Email Address >>",
+//    password: "<< Password >>"
+//}, function(error, userData) {
+//    if (error) {
+//        switch (error.code) {
+//            case "EMAIL_TAKEN":
+//                console.log("The new user account cannot be created because the email is already in use.");
+//                break;
+//            case "INVALID_EMAIL":
+//                console.log("The specified email is not a valid email.");
+//                break;
+//            default:
+//                console.log("Error creating user:", error);
+//        }
+//    } else {
+//        console.log("Successfully created user account with uid:", userData.uid);
+//    }
+//});
+
+//ref.authWithPassword({
+//    email    : "<< Email Address >>",
+//    password : "<< Password >>"
+//}, function(error, authData) {
+//    if (error) {
+//        console.log("Login Failed!", error);
+//    } else {
+//        console.log("Authenticated successfully with payload:", authData);
+//    }
+
+});
